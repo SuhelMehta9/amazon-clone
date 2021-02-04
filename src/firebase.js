08d141e28b55f1034d73eeef3519cf186358ebc8
Suhel Mehta <suhelmehta@outlook.com>
2021-02-04 15:51:56 +0530

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3DidFK_ZejxjM_rx6UtwXPWbfmMWiG8A",
+    authDomain: "clone-79647.firebaseapp.com",
+    projectId: "clone-79647",
+    storageBucket: "clone-79647.appspot.com",
+    messagingSenderId: "698900380614",
+    appId: "1:698900380614:web:1ffe80d2ae78d3392d90c7"
+  };
